d0d80b3f009b5f1c53addc08650161f5a0155159
andresrivero <andres@rive.ro>
2017-02-01 08:28:13 -0800

    First try nothing special

--- src/GomokuPlayer.java
+++ src/GomokuPlayer.java
@@ -1,13 +1,87 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class GomokuPlayer {
-
-	public GomokuPlayer() {
-		// TODO Auto-generated constructor stub
+	
+	protected Socket socket;			
+    protected PrintWriter output;            
+    protected BufferedReader input;
+    protected String setup;
+    public static final int DEFAULTPORT = 17033;    
+    
+	public GomokuPlayer(String host, int port) {
+			openSocket(host, port);
 	}
+	
+	public void openSocket(String host, int port) {
+        
+		try {
+	    
+            socket = new Socket(host, port);
+            System.out.println("Opened port");
+            output = new PrintWriter(socket.getOutputStream(), true); 
+            System.out.println("setup port output");
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            setup = null;
+            System.out.print(input.readLine() + "\n");
+            
+            for (int x = 0; x < 11; x ++ ) {
 
+                System.out.print(x + ":" + input.readLine() + "\n");
+            }
+
+            System.out.print(input.readLine() + "\n");
+            System.out.println("Setup port input buffered reader ");
+            
+        } catch (UnknownHostException e) {
+            System.err.println("Can't connect to host: " + host);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Failed I/O for: " + host);
+            System.exit(1);
+        }
+    }
+	public List<String> getBoard() throws Exception {
+		
+		String line = null;
+		List<String> board = new ArrayList<String>();
+		  for (int x = 0; x < 11; x ++ ) {
+			  line = input.readLine();
+              board.add(line);
+          }
+		System.out.println("Got board");
+		return board;
+	}
+	
+	public void sendMove(String move) throws Exception {
+		
+			output.println(move + "\n");
+	}
+	
+	public void play () {
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		GomokuPlayer ai = new GomokuPlayer("localhost", DEFAULTPORT);
+		List<String> board = null;
+		Scanner scan = new Scanner(System.in);
+		
+		while (true) {
+			try {
+				board = ai.getBoard();
+				System.out.println("Enter move: ");
+				ai.sendMove(scan.nextLine());
+				System.out.println("--------------");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				
+		}
 	}
 
 }
