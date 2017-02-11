3ecea719084d0ff02991f7e4e0219260992982be
andresrivero <andres@rive.ro>
2017-02-10 19:17:51 -0800

    ABNode extending

--- src/ABPrune.java
+++ src/ABPrune.java
@@ -4,8 +4,8 @@ import java.util.Enumeration;
 import javax.swing.tree.*;
 
 public class ABPrune {
-	ABNode root;
-	DefaultMutableTreeNode rut;
+
+
 	class Point  {
 		int row;
 		int col;
@@ -18,41 +18,16 @@ public class ABPrune {
 		}
 	}
 	
-	class ABNode implements MutableTreeNode {
-		
-		
-		public DefaultMutableTreeNode node;
-		
-		public ABNode(String type, Point score) {
-			node = new DefaultMutableTreeNode();
+	class ABNode extends DefaultMutableTreeNode {
+		protected int alpha;
+		protected int beta;
+		protected String type;
+		public ABNode(String type, int alpha, int beta, Object userObject, boolean allowsChildren) {
+			super(userObject, allowsChildren);
+			this.type = type;
+			this.alpha = alpha;
+			this.beta = beta;
 			
-			switch (type) {
-			case "leaf": {
-				node.setAllowsChildren(false);
-				node.setUserObject(score);
-				break;
-			}
-			case "root": {
-				//this.node.setAllowsChildren(true);
-
-				//this.node.setUserObject(null);
-				break;
-			}
-			case "min": {
-				node.setAllowsChildren(true);
-				node.setUserObject(null);
-				break;
-			}
-			case "max": {
-				node.setAllowsChildren(true);
-				node.setUserObject(null);
-				break;
-			}
-			
-			default: {
-				break;
-			}
-			}
 		}
 		
 		public Point Max() {
@@ -72,97 +47,118 @@ public class ABPrune {
 			return null;
 		}
 
-		@Override
-		public TreeNode getChildAt(int childIndex) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public int getChildCount() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public TreeNode getParent() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public int getIndex(TreeNode node) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public boolean getAllowsChildren() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean isLeaf() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public Enumeration children() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public void insert(MutableTreeNode child, int index) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void remove(int index) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void remove(MutableTreeNode node) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setUserObject(Object object) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void removeFromParent() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setParent(MutableTreeNode newParent) {
-			// TODO Auto-generated method stub
-			
-		}
+//		@Override
+//		public TreeNode getChildAt(int childIndex) {
+//			// TODO Auto-generated method stub
+//			return null;
+//		}
+//
+//		@Override
+//		public int getChildCount() {
+//			// TODO Auto-generated method stub
+//			return 0;
+//		}
+//
+//		@Override
+//		public TreeNode getParent() {
+//			// TODO Auto-generated method stub
+//			return null;
+//		}
+//
+//		@Override
+//		public int getIndex(TreeNode node) {
+//			// TODO Auto-generated method stub
+//			return 0;
+//		}
+//
+//		@Override
+//		public boolean getAllowsChildren() {
+//			// TODO Auto-generated method stub
+//			return false;
+//		}
+//
+//		@Override
+//		public boolean isLeaf() {
+//			// TODO Auto-generated method stub
+//			return false;
+//		}
+//
+//		@Override
+//		public Enumeration children() {
+//			// TODO Auto-generated method stub
+//			return null;
+//		}
+//
+//		@Override
+//		public void insert(MutableTreeNode child, int index) {
+//			// TODO Auto-generated method stub
+//			
+//		}
+//
+//		@Override
+//		public void remove(int index) {
+//			// TODO Auto-generated method stub
+//			
+//		}
+//
+//		@Override
+//		public void remove(MutableTreeNode node) {
+//			// TODO Auto-generated method stub
+//			
+//		}
+//
+//		@Override
+//		public void setUserObject(Object object) {
+//			// TODO Auto-generated method stub
+//			
+//		}
+//
+//		@Override
+//		public void removeFromParent() {
+//			// TODO Auto-generated method stub
+//			
+//		}
+//
+//		@Override
+//		public void setParent(MutableTreeNode newParent) {
+//			// TODO Auto-generated method stub
+//			
+//		}
 	}
 	
 	public ABPrune() {
 		
-		ArrayList array = new ArrayList();
+		ArrayList<Point> array = new ArrayList<Point>();
 		for (int x = 1; x < 10; x++) {
 			array.add(new Point(x,x,x));
 		}
-		root = new ABNode("root", null);
+		ABNode root = new ABNode("root", Integer.MIN_VALUE, Integer.MAX_VALUE, null, true);
 		
-		root.node.insert(new ABNode("min",null),0);
-		root.node.insert(new ABNode("min",null),1);
 
-		System.out.println(root.node.getChildCount());
+				
+		root.add(new ABNode("min", 0, 0, null, true));
+		root.add(new ABNode("min", 0, 0, null, true));
+		root.add(new ABNode("min", 0, 0, null, true));
+
+		ABNode tmp = root;
+		Enumeration children = tmp.children();
+		System.out.println(root.getChildCount());
+		
+		while (children.hasMoreElements()) {
+			tmp = (ABNode) children.nextElement();
+			
+			tmp.add(new ABNode("leaf", 0, 0, new Point(array.listIterator().next().value, 0, 0), false));
+			tmp.add(new ABNode("leaf", 0, 0, new Point(array.listIterator().next().value, 0, 0), false));
+			tmp.add(new ABNode("leaf", 0, 0, new Point(array.listIterator().next().value, 0, 0), false));
+
+		}
+		 System.out.println( tmp.getChildCount());
+		
+		System.out.println(tmp.getUserObject());
+		System.out.println(tmp.getLevel());
+		tmp = (ABNode) tmp.getFirstChild();
+		System.out.println(tmp.getLevel());
+
 		
 		
 		
